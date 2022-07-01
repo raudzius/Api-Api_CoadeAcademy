d349cd38da4c8da8d91230b150f6fac14630b666
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-07-01 13:13:37 +0300

    idk

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 const form = document.forms[0];
+const apiOl = document.querySelector('ol');
+let apiList;
+
+fetch('https://api.publicapis.org/entries')
+  .then(res => res.json())
+  .then(data => {
+    apiList = data.entries;
+});
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const form = event.target;
-  const userInput = form.elements.filter.value;
-  const apiOl = document.querySelector('ol');
+  const apiOlChildren = apiOl.children;
+  console.log(apiOlChildren);
+  const filteredArray = [...apiOlChildren].filter(apiArrayObj => {
+    return Object.values(apiArrayObj).filter(apiArrayObjValue => {
+      return apiArrayObjValue.toString().includes(userInput);
+    }).length;
+  });
 
-  fetch('https://api.publicapis.org/entries')
-    .then(res => res.json())
-    .then(data => {
-      const apiArray = data.entries;
-      const filteredApiArray = apiArray.filter(apiArrayObj => {
-        return Object.values(apiArrayObj).filter(apiArrayObjValue => {
-          return apiArrayObjValue.toString().includes(userInput);
-        }).length;
-      });
-
-      filteredApiArray.forEach(api => {
-        const apiObjLi = document.createElement('li');
-        const apiPropertyUl = document.createElement('ul');
-        apiObjLi.style.margin = '10px';
-
-        for (key in api) {
-          const apiPropertyLi = document.createElement('li');
-          apiPropertyLi.textContent = `${[key]} : ${api[key]}`;
-          apiPropertyUl.append(apiPropertyLi);
-        }
-        apiObjLi.append(apiPropertyUl);
-        apiOl.append(apiObjLi);
-      });
-    });
+  console.log(filteredArray);
 });
