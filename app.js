6e94a2a88bc492c2a154268386afd36862dbb02a
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-07-01 16:38:06 +0300

    Refactor/Fix variable names and Bugs
    - remove searches uppercase sensitivity
    
    - change variable names
    
    - move api li margin to css

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const form = document.forms[0];
 const apiOl = document.querySelector('ol');
-let apiList;
+let apis;
 
 fetch('https://api.publicapis.org/entries')
   .then(res => res.json())
   .then(data => {
-    apiList = data.entries;
-});
+    apis = data.entries;
+    apis.forEach(api => {
+      const apiLi = document.createElement('li');
+      const apiItemUl = document.createElement('ul');
+      for (key in api) {
+        const apiItemLi = document.createElement('li');
+        apiItemLi.textContent = `${key} : ${api[key]}`;
+        apiItemUl.append(apiItemLi);
+      }
+      apiLi.append(apiItemUl);
+      apiOl.append(apiLi);
+    });
+
+    document.querySelector('input[disabled]').disabled = false;
+  });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const form = event.target;
-  const apiOlChildren = apiOl.children;
-  console.log(apiOlChildren);
-  const filteredArray = [...apiOlChildren].filter(apiArrayObj => {
-    return Object.values(apiArrayObj).filter(apiArrayObjValue => {
-      return apiArrayObjValue.toString().includes(userInput);
-    }).length;
+  const usrInput = form.elements.filter.value.toLowerCase();
+
+  const filterRemainIndexArray = apis.map((api, index) => {
+    const noMatchingValue = !Object.values(api).filter(apiKeyValue =>
+      apiKeyValue.toString().toLowerCase().includes(usrInput)
+    ).length;
+
+    if (noMatchingValue) {
+      return index;
+    }
   });
 
-  console.log(filteredArray);
+  [...apiOl.children].forEach((apiLi, index) => {
+    apiLi.style.display = 'list-item';
+    if (filterRemainIndexArray.includes(index)) {
+      apiLi.style.display = 'none';
+    }
+  });
 });
